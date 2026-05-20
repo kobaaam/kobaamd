671c1e4bcace525558c54a275b6d833686547ee8
kobaaam <kobaaam@users.noreply.github.com>
2026-05-20 14:38:49 +0900

    KMD-71: wire Tree-sitter highlighting into editor

--- Sources/Views/Editor/NSTextViewWrapper.swift
+++ Sources/Views/Editor/NSTextViewWrapper.swift
@@ -83,13 +83,20 @@ struct EditorObserver: NSViewRepresentable {
     final class Coordinator: NSObject {
         private var scrollObserver: Any?
         private var selectionObserver: Any?
+        private var textDidChangeObserver: Any?
         private var insertSnippetObserver: Any?
         private var eventMonitor: Any?
+        private let highlightService: HighlightServiceProtocol
         weak var textViewRef: NSTextView?
         weak var appViewModel: AppViewModel?
         var currentTheme: ColorTheme = AppState.shared.selectedTheme
         var highlightColor: NSColor { currentTheme.editorCurrentLineHighlight }
         private var lastHighlightedRange: NSRange = NSRange(location: NSNotFound, length: 0)
+
+        @MainActor
+        init(highlightService: HighlightServiceProtocol? = nil) {
+            self.highlightService = highlightService ?? TreeSitterHighlightService()
+        }
 
         func attach(to view: NSView, scrollRatio: Binding<Double>) {
             var current: NSView? = view
@@ -170,6 +177,20 @@ struct EditorObserver: NSViewRepresentable {
                 MainActor.assumeIsolated {
                     self.updateAIOverlayPosition(in: tv)
                 }
+            }
+            textDidChangeObserver = NotificationCenter.default.addObserver(
+                forName: NSText.didChangeNotification,
+                object: tv,
+                queue: .main
+            ) { [weak self, weak tv] _ in
+                guard let self, let tv else { return }
+                MainActor.assumeIsolated {
+                    self.highlightEditedText(in: tv)
+                }
+            }
+
+            MainActor.assumeIsolated {
+                applySyntaxHighlight(in: tv)
             }
             highlightCurrentLine(in: tv)
             MainActor.assumeIsolated {
@@ -352,11 +373,51 @@ struct EditorObserver: NSViewRepresentable {
 
         @MainActor
         func refreshHighlightForThemeChange(in tv: NSTextView) {
+            applySyntaxHighlight(in: tv)
             if let lm = tv.layoutManager, lastHighlightedRange.location != NSNotFound {
                 lm.removeTemporaryAttribute(.backgroundColor, forCharacterRange: lastHighlightedRange)
             }
             lastHighlightedRange = NSRange(location: NSNotFound, length: 0)
             highlightCurrentLine(in: tv)
+        }
+
+        @MainActor
+        func applySyntaxHighlight(
+            in tv: NSTextView,
+            editedRange: NSRange? = nil,
+            changeInLength: Int = 0
+        ) {
+            guard let textStorage = tv.textStorage else { return }
+
+            if let editedRange, editedRange.location != NSNotFound {
+                highlightService.applyIncrementalHighlight(
+                    textStorage: textStorage,
+                    editedRange: editedRange,
+                    changeInLength: changeInLength
+                )
+                return
+            }
+
+            highlightService.highlight(textStorage)
+        }
+
+        @MainActor
+        func highlightEditedText(in tv: NSTextView) {
+            guard let textStorage = tv.textStorage else { return }
+
+            let editedRange = textStorage.editedRange
+            let changeInLength = textStorage.changeInLength
+
+            if editedRange.location == NSNotFound {
+                applySyntaxHighlight(in: tv)
+                return
+            }
+
+            applySyntaxHighlight(
+                in: tv,
+                editedRange: editedRange,
+                changeInLength: changeInLength
+            )
         }
 
         private func highlightCurrentLine(in tv: NSTextView) {
@@ -408,7 +469,7 @@ struct EditorObserver: NSViewRepresentable {
         }
 
         deinit {
-            [scrollObserver, selectionObserver, insertSnippetObserver].compactMap { $0 }.forEach {
+            [scrollObserver, selectionObserver, textDidChangeObserver, insertSnippetObserver].compactMap { $0 }.forEach {
                 NotificationCenter.default.removeObserver($0)
             }
             if let eventMonitor { NSEvent.removeMonitor(eventMonitor) }
